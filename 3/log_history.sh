183676c96ca5cf5da3e0bb934f238c482f9b4f67
Mariusz Boike <sp4mjkm@hotmail.com>
2010-11-01 23:09:13 +0100

    zadanka 1-6

---
+++ 3/log_history.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "\\033[31mPodaj nazwe uzytkownika:\\033[0m"
+read nazwa_uzytkownika 
+
+echo -e "\\033[31mPodaj date:\\033[0m" 
+read data
+
+szukaj=`last "$nazwa_uzytkownika" | grep "$data" | wc -l`
+
+echo -e "\\033[31m=============================================================
+Nazwa uzytkownika: $nazwa_uzytkownika
+Data: $data
+Uzytkownik $nazwa_uzytkownika zalogowal sie $szukaj razy dnia $data
+=============================================================\\033[0m"
