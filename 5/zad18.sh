68e7d7b739ffcab0fba612cc6e8f69fac97d8ccd
Mariusz Boike <sp4mjkm@hotmail.com>
2010-11-23 11:43:32 +0100

    zadania z lab5

---
+++ 5/zad18.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -n "Podaj ilość użytkowników:"
+read users 
+
+wynik=`./genlogs.sh $users | cut -c38-47 | sort | uniq -c`
+
+echo "$wynik";
---
+++ 5/zad20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Podaj ilość użytkowników:"
+read users
+
+wynik=`./genlogs.sh $users | cut -c38-47 | sort | uniq -c | sort -r | 
+head -1`
+
+echo "$wynik";
