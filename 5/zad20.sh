b3fabf2780ae6401afb9221e477df3b4040f46be
Mariusz Boike <sp4mjkm@hotmail.com>
2010-12-11 20:55:27 +0100

    lab2 - sys info

--- 5/zad20.sh
+++ 5/zad20.sh
@@ -2,7 +2,6 @@
 echo "Podaj ilość użytkowników:"
 read users
 
-wynik=`./genlogs.sh $users | cut -c38-47 | sort | uniq -c | sort -r | 
-head -1`
+wynik=`./genlogs.sh $users | cut -c38-47 | sort | uniq -c | sort -r | head -1`
 
 echo "$wynik";
